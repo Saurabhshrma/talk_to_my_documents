b06a13a38e6ff86a0ea85ce4ab1d3701c00070d0
Saurabhshrma <saurabh1346.ss@gmail.com>
2026-04-19 19:20:49 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='talk_to_my_documents',
-    version='0.1',
+    version='0.15',
     author='Saurabh Sharma',
     packages=find_packages(),
 )
